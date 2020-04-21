59565ddc286860856a422bf5ea06cb2bcee41a10
Jasper van den Bosch <japsai@gmail.com>
2020-04-21 16:25:47 +0200

    printing fmr recorded times for comparison

--- bids_logs.py
+++ bids_logs.py
@@ -1,7 +1,7 @@
 """
 Organize and convert the event log files to BIDS format
 """
-import subprocess, glob, shutil, os
+import subprocess, glob, shutil, os, time, json, datetime
 from os.path import join, expanduser, basename, isdir
 from subject_ids import subject_ids
 from logfile import PresentationLogfile
@@ -13,18 +13,25 @@ bidsdir = join(root, 'BIDS')
 assert isdir(bidsdir)
 
 logfiles_localizer = glob.glob(join(root, 'logfiles_localizer', '*.log'))
+logfiles_experiment = glob.glob(join(root, 'logfiles_experiment', '*.log'))
+all_logfiles = logfiles_localizer + logfiles_experiment
 
 ## loop over subjects
 for old_id, sub in subject_ids.items():
-    print(f'\nSubject {sub} ({old_id})')
-    loc_matches = list(filter(lambda f: basename(f)[:2] == old_id, logfiles_localizer))
-    for fpath in loc_matches:
+    print(f'\n\nSubject {sub} ({old_id})')
+
+    sub_logfiles = list(filter(lambda f: basename(f)[:2] == old_id, all_logfiles))
+    print(f'\t{len(sub_logfiles)} log files.')
+
+    for fpath in sub_logfiles:
         logfile = PresentationLogfile(fpath)
         logfile.read()
         print(f'\t\tLogfile created {logfile.created}')
-    if not len(loc_matches) == 1:
-        print(f'\t[!] Found {len(loc_matches)} logfiles.')
-## for localizer, print info on fmri created timing
-## for localizer, print info on logfile created timing
-## for exp, print info on fmri created timing
-## for exp, print info on logfile created timing
+
+    sub_fmrfiles = glob.glob(join(bidsdir, f'sub-{sub}', 'func', '*_bold.json'))
+    print(f'\t{len(sub_fmrfiles)} fmr files.')
+    for fpath in sub_fmrfiles:
+        with open(fpath) as fhandle:
+            fmr_meta = json.load(fhandle)
+        aq_time = datetime.datetime.strptime(fmr_meta['AcquisitionTime'], '%H:%M:%S.%f')
+        print(f'\t\tFmr created {aq_time}')
