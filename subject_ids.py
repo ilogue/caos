e4737a672a5a171cc5fcf8df3f687101ba06179b
Jasper van den Bosch <japsai@gmail.com>
2020-04-21 12:45:52 +0200

    added key for converting old to new subject ids

---
+++ subject_ids.py
@@ -0,0 +1,26 @@
+"""Dictionary for the conversion between old identifiers and new subject numeric IDs.
+"""
+
+subject_ids = {
+    'SG': 1,
+    'SE': 2,
+    'RL': 3,
+    'PF': 4,
+    'NS': 5,
+    'MN': 6,
+    'MM': 7,
+    'MH': 8,
+    'MD': 9,
+    'LS': 10,
+    'LG': 11,
+    'KV': 12,
+    'KD': 13,
+    'JD': 14,
+    'JB': 15,
+    'FL': 16,
+    'EW': 17,
+    'DD': 18,
+    'CS': 19,
+    'CA': 20,
+    'AS': 21
+}
